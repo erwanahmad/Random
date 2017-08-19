9b23844732d7a08c6b08defd95e6f29f2a43f305
Erwan Ahmad Ardiansyah <ardiansyaherwan40@gmail.com>
2017-08-19 17:19:34 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(StaticRandom.Next());
+            }
+        }
+
+        private class StaticRandom
+        {
+            internal static bool Next()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
